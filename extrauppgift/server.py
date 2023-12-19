92987df4ded97529b08bb8f07aaa792326df89a9
Jimmy <berlin.jimmy@gmail.com>
2023-12-19 14:40:29 +0100

    added some formatting and printing in serverscript

--- extrauppgift/server.py
+++ extrauppgift/server.py
@@ -1,6 +1,9 @@
 import paho.mqtt.client as mqtt
 from typing import Any, Optional
+import os
 
+data = {}
+log = []
 
 def on_connect(client, userdata, flags, rc):
     """ Anropas när vi ansluter till brokern """
@@ -13,8 +16,27 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, message):
     """ Anropas när vi får ett meddelande från brokern """
-    print(f"Topic: {message.topic}, Message: {message.payload.decode('utf-8')}")
-
+    # print(f"Topic: {message.topic}, Message: {message.payload.decode('utf-8')}")
+    try:
+        m = message.payload.decode().split(":")
+        data[m[0]] = m[2]
+    except:
+        log.append(str(userdata) + ":" + message.payload.decode())
+        if len(log) >= 10:
+            log.remove(0)
+    
+    os.system("cls")
+    print("===========================================")
+    print("Stats")
+    print("===========================================")
+    for k, v in data.items():
+        print(k + ": " + v)
+    print()
+    print("===========================================")
+    print("People with problems end up here:")
+    print("===========================================")
+    for l in log:
+        print(l)
 
 def main():
     client = mqtt.Client()
@@ -29,8 +51,6 @@ def main():
     # Lyssna på trafik
     client.loop_forever()
     
-
-
 
 if __name__ == '__main__':
     main()
